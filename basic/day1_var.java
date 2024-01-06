d679b05d8d8741519e1917ac225f7a283e8a5033
wc_earlymor <1162907534@qq.com>
2024-01-06 21:27:48 +0800

    day1

--- basic/day1_var.java
+++ basic/day1_var.java
@@ -8,15 +8,36 @@ class Test1{
         System.out.println("1"+(2+3)); // 15
     }
 }
+
 class Test2{
     public static void main(String[]args){
-        /* 错误: 不兼容的类型: int无法转换为boolean
+        /* 
+            错误: 不兼容的类型: int无法转换为boolean
             if(2){
             System.out.println(true);
         } */
         if(true){
             System.out.println(true);
         }
-        
+
+    }
+}
+
+class Test3{
+
+    public static void main(String[]args){
+        // 字面量
+        int a = 10;
+        long b = 100L;
+        // int c = 10L;错误: 不兼容的类型: 从long转换到int可能会有损失
+        int c = (int)10L;
+        int d = (int)2147483648L;
+        // int e = 2147483648; 错误: 整数太大
+        System.out.println("a="+a); // a=10
+        System.out.println("b="+b); // b=100
+        System.out.println("c="+c); // c=10
+        System.out.println("d="+d); // d=-2147483648
+        // System.out.println("e="+e);
+
     }
 }
